5f86c0da1ea2ef18cac5fb143cf29c5347770880
AlexYshkevich <bor_bor_1997@mail.ru>
2016-12-05 19:35:47 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,81 +1,26 @@
-#include "SortFile.cpp"
+#include <sort.cpp>
 #include <catch.hpp>
-#include <iostream>
 #include <string>
+#include <iostream>
 #include <fstream>
 using namespace std;
-SCENARIO("sort", "[sort]")
-{
-  SortFile obj("sort.txt", 1, "fl.txt");
-//ifstream file("sort_small.txt");
- // ifstream out("fl.txt");
- bool p=true;
- //string s1,s2;
- //while (!file.eof()&&!out.eof()){
-// getline(file,s1);
-//  getline(out,s2);
- // if (s1==s2 || s1==""||s2==""){
-///  p=true;
- // }
-  // else
- //  {
-  //   p=false;
-  //   break;
- //  }
- 
-// }
- // file.close();
- // out.close();
-  REQUIRE(p==true);
-}
-SCENARIO("sort2", "[sort2]")
-{
-  SortFile obj("15.txt", 4, "fl2.txt");
-//ifstream file2("sort_15.txt");
- // ifstream out2("fl2.txt");
- bool p=true;
-// string s1,s2;
-//// while (!file2.eof()&&!out2.eof()){
-// getline(file2,s1);
- // getline(out2,s2);
- // if (s1==s2 || s1==""||s2==""){
- // p=true;
- // }
-//   else
-//   {
-   //  p=false;
-   //  break;
-  // }
- 
-// }
- // file2.close();
- // out2.close();
-  REQUIRE(p==true);
-}
 
-SCENARIO("sort3", "[sort3]")
+SCENARIO("zzz", "[zzz]")
 {
-  SortFile obj("32.txt", 17, "fl4.txt");
-//ifstream file3("sort_32.txt");
- // ifstream out3("fl4.txt");
- bool p=true;
-// string s1,s2;
-// long int i=0;
-// while (!file3.eof()&&!out3.eof()){
-// getline(file3,s1);
-//  getline(out3,s2);
- //  i++;
- // if (s1==s2 || s1==""|| s2=="")
-//  {
- 
-  //   p=true;
-     
- 
- 
-// }
-// }
- // file3.close();
- // out3.close();
-  REQUIRE(p==true);
-
-}
+  sort_it("in.txt", "out.txt", 120);
+	string *a = new string[21], *b = new string[21];
+	bool x = false;
+	ifstream f1("out.txt"), f2("test.txt");
+	for (int i = 0; i < 21; ++i)
+	{
+		getline(f1, a[i]);
+		getline(f2, b[i]);
+	}
+	size_t x_ = 0;
+	for (int i = 0; i < 21; ++i)
+	{
+		if (a[i] == b[i]) ++x_;
+	}
+	if (x_ == 21) x = true;
+  REQUIRE(x);
+} 
