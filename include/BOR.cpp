6967885214aea6d4bde779b4b76ea291a1005d7f
AlexYshkevich <bor_bor_1997@mail.ru>
2016-12-05 20:20:42 +0300

    Update BOR.cpp

--- include/BOR.cpp
+++ include/BOR.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <vector>
 #include <algorithm>
 #include <queue>
-#include <fstream>
-#include <string>
 
 using namespace std;
 
