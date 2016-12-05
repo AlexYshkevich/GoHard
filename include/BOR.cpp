3a1b8e930e8d96da77e2eee8d2a8dec49c4b1699
AlexYshkevich <bor_bor_1997@mail.ru>
2016-12-05 19:29:04 +0300

    Update BOR.cpp

--- include/BOR.cpp
+++ include/BOR.cpp
@@ -1,172 +1,77 @@
 #include <iostream>
-#include "BOR.h"
-#include <stdexcept>
-#include <map> 
-#include <iterator>
-#ifndef SORT_CPP
-#define SORT_CPP
-auto str_to_vec(string str) -> vector<string>
-{
-	std::vector<string> buf;
-	if (str == "")
-		return buf;
-	size_t k = 0;
-	for (size_t i = 0; i < str.length(); i++)
-	{
-		if (str[i] == ' ')
-		{
-			if (str[k] != ' ')
-			{
-				buf.push_back(str.substr(k, i - k));
-			}
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
 
-			k = i + 1;
-			continue;
-		}
-		if (i == str.length() - 1)
-		{
-			buf.push_back(str.substr(k, i - k + 1));
-		}
-	}
-	return buf;
-}
-auto new_file(std::string file_name, uint_fast64_t file_size) -> void
+struct s_i
 {
-	std::string names[] = { "Ibragim", "Anton", "Vika", "Dima", "Kozan", "Daler", "Aleksandr", "Oleg" };
-	std::string surnames[] = { "Nurmagomedov", "Perunov", "Bardina", "Rybko", "Geopika", "Boltabaev", "Nikitin", "Velikiy", "Magomedov", "Vershinin", "Gigin" };
-	std::ofstream file;
-	file.open(file_name);
-	if (file.good())
+	string s;
+	ifstream *f;
+	s_i(const string& s_,ifstream* f_) : s(s_), f(f_){}
+	bool operator < (const s_i& s_i_) const
 	{
-		while (file.tellp() < file_size)
-		{
-			file << surnames[rand() % 11].data() << " "
-				<< names[rand() % 8].data() << " "
-				<< 1950 + rand() % 2016 << std::endl;
-		}
-		file.close();
+		return (s > s_i_.s);
 	}
-	else
-	{
-		throw std::bad_exception();
-	}
-}
-SortFile::~SortFile()
-{
-	file_names.clear();
-}
-SortFile::SortFile(string name_main_file, size_t buffer_, string name_out_file) :file(name_main_file), buffer(buffer_*1024*1024), s_out(name_out_file), count_of_files(0), closed_files(0)
-{
-	if (file.is_open())
-	{
-		division();
-	}
-}
-auto SortFile::make_file(string name_file)->void
-{
-	file_names.push_back(name_file); 
-	ofstream temp(name_file);
-	for (auto i : lines)
-	{
-		temp << i << endl;
-	}
-	temp.close();
-	lines.clear();
-}
-auto SortFile::file_size(string name_file)->size_t
-{
-	long fsize;
-	ifstream temp(name_file);
-	temp.seekg(0, ios::end);
-	fsize = temp.tellg();
-	temp.close();
-	return fsize;
-}
-auto SortFile::out_file(string line)->void
-{
-	ofstream file(s_out, ios::app);
-	file << line << endl;
-	file.close();
-
-}
-auto SortFile::remove_temp_files()->void
-{
-	for (int i = 0; i < file_names.size(); ++i)
-	{
-		if (remove(file_names[i].c_str()) == -1)
-		{
-			throw;
-		}
-		else
-		{
-			cout << "So very good, NOT BED";
-		}
-	}
-
-}
-auto SortFile::sort()->void
-{
-	ifstream *streams = new ifstream[count_of_files];
-	string *top_line = new string[count_of_files];
-	for (int i = 0; i < count_of_files; ++i)
-	{
-		streams[i].open(file_names[i]);
-		getline(streams[i], top_line[i]);
-		map.insert(pair<string, size_t>(top_line[i], i));
-	}
-
-	while (!map.empty())
-	{
-		auto it = map.begin();
-		int n = (*it).second;
-		out_file((*it).first);
-		if (!streams[n].eof())
-		{
-			getline(streams[n], top_line[n]);
-			map.erase(map.begin());
-			map.insert(pair<string, size_t>(top_line[n], n));
-		}
-		else
-		{
-			closed_files++;
-			streams[n].close();
-			map.erase(map.begin());
-	
-		}
-	}
-
-}
-
-auto SortFile::division()->void
-{
-	string line_of_file;
-	size_t temp_size_files = 0;
-	while (!file.eof())
-	{
-		getline(file, line_of_file);
-		temp_size_files += line_of_file.size();
-		if (temp_size_files <= buffer)
-		{
-			lines.emplace(line_of_file);
-		}
-		else
-		{
-			count_of_files++;
-			make_file(to_string(count_of_files) + ".txt");
-			lines.emplace(line_of_file);
-			temp_size_files = line_of_file.size();
-		}
-	}
-	file.close();
-
-	if (lines.size())
-	{
-		count_of_files++;
-		make_file(to_string(count_of_files) + ".txt");
-	}
-
-
-	sort();
 };
 
-#endif
+void sort_it(const string input_name, const string output_name, const unsigned long int mem_size)
+{
+	ifstream fin(input_name);
+	if (!fin.is_open()) throw("file_not_open");
+	ofstream fout(output_name);
+	size_t k = 0;
+	while (!fin.eof())
+	{
+		ofstream fout_(to_string(k + 1) + ".txt");
+		vector<string> v; string s;
+		for (unsigned long int size = 0; (size + sizeof(string)) <= mem_size; size += sizeof(string))
+		{
+			getline(fin, s);
+			v.push_back(s);
+		}
+		sort(v.begin(), v.end());
+		for (auto i : v)
+		{
+			if (i != "") fout_ << i<<endl;
+		}
+		++k;
+		fout_.close();
+	}
+	fin.close();
+	priority_queue<s_i> pq;
+	for (size_t i = 0; i < k; ++i)
+	{
+		ifstream* f_ = new ifstream(to_string(i + 1) + ".txt");
+		string str;
+		getline(*f_, str);
+		s_i si(str, f_);
+		pq.push(si);
+	}
+	while (!pq.empty())
+	{
+		s_i si = pq.top();
+		pq.pop();
+		if (si.s != "")
+		{
+			fout << si.s;
+			fout << endl;
+		}
+		if (!(*si.f).eof())
+		{
+			getline(*si.f, si.s);
+			pq.push(si);
+		}
+		else
+		{
+			(*(si.f)).close();
+		}
+	}
+	for (size_t i = 0; i < k; ++i)
+	{
+		remove((to_string(i + 1) + ".txt").c_str());
+	}
+	fout.close();
+}
