0d0da1063ffe276f1fab50b98bbf2c69e09fbab7
AlexYshkevich <bor_bor_1997@mail.ru>
2016-12-06 23:44:23 +0300

    Update sort.cpp

--- include/sort.cpp
+++ include/sort.cpp
@@ -1,77 +1,150 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <queue>
+#include <iostream> 
+#include <fstream> 
+#include <string> 
+#include <vector> 
+#include <locale> 
+#include <iterator> 
+#include <algorithm> 
+#include <cstdio> 
+#include <queue> 
+
 using namespace std;
 
-struct s_i
-{
-	string s;
+struct A {
+public:
 	ifstream *f;
-	s_i(const string& s_,ifstream* f_) : s(s_), f(f_){}
-	bool operator < (const s_i& s_i_) const
+	string str;
+	A(const string& s, ifstream* f_) : str(s), f(f_) {}
+	bool operator < (const A& s) const
 	{
-		return (s > s_i_.s);
+		return (str > s.str);
 	}
 };
 
-void sort_it(const string input_name, const string output_name, const unsigned long int mem_size)
-{
-	ifstream fin(input_name);
-	if (!fin.is_open()) throw("file_not_open");
-	ofstream fout(output_name);
-	size_t k = 0;
-	while (!fin.eof())
-	{
-		ofstream fout_(to_string(k + 1) + ".txt");
-		vector<string> v; string s;
-		for (unsigned long int size = 0; (size + sizeof(string)) <= mem_size; size += sizeof(string))
-		{
-			getline(fin, s);
-			v.push_back(s);
-		}
-		sort(v.begin(), v.end());
-		for (auto i : v)
-		{
-			if (i != "") fout_ << i<<endl;
-		}
-		++k;
-		fout_.close();
+class B {
+public:
+	B(string name_main_file,size_t buff_size);
+	auto division()->void;
+	auto file_size(string name_file)->size_t;
+	auto make_file(string name_file)->void;
+	auto file_sort()->void;
+	auto remove_temp_files()->void;
+	~B();
+private:
+	fstream file;
+	size_t buffer, count_of_files, closed_files;
+	vector<string> lines;
+	vector<string> file_names;
+	priority_queue<A> end_sorting;
+};
+
+
+
+inline B::~B() {
+	file_names.clear();
+}
+
+inline B::B(string name_main_file,size_t buff_size) :file(name_main_file), buffer(buff_size), count_of_files(0), closed_files(0) {
+	if (file.is_open()) {
+		division();
 	}
-	fin.close();
-	priority_queue<s_i> pq;
-	for (size_t i = 0; i < k; ++i)
+};
+
+inline auto B::make_file(string name_file)->void {
+	file_names.push_back(name_file);
+	std::sort(lines.begin(), lines.end());
+	ofstream temp(name_file);
+	for (auto i : lines)
 	{
-		ifstream* f_ = new ifstream(to_string(i + 1) + ".txt");
-		string str;
+		temp << i;
+		if (i != *(--lines.end())) temp << endl;
+	}
+	temp.close();
+	lines.clear();
+}
+
+inline auto B::file_size(string name_file)->size_t {
+	long fsize;
+	ifstream temp(name_file);
+	temp.seekg(0, ios::end);
+	fsize = temp.tellg();
+	temp.close();
+	return fsize;
+}
+
+
+
+inline auto B::remove_temp_files()->void {
+	for (int i = 0; i < file_names.size(); ++i) {
+		if (remove(file_names[i].c_str()) == -1) {
+			throw;
+		}
+		else {
+			cout << "Gj";
+		}
+	}
+
+}
+
+
+
+
+inline auto B::file_sort()->void {
+	ofstream f12("out.txt");
+	
+	string str;
+	for (int i = 0; i < count_of_files; ++i) {
+		ifstream* f_ = new ifstream(file_names[i]);
 		getline(*f_, str);
-		s_i si(str, f_);
-		pq.push(si);
+		A ff(str, f_);
+		end_sorting.push(ff);
 	}
-	while (!pq.empty())
-	{
-		s_i si = pq.top();
-		pq.pop();
-		if (si.s != "")
+	
+	while (!end_sorting.empty()) {
+		A ff = end_sorting.top();
+		end_sorting.pop();
+		if (ff.str != "") f12 << ff.str << endl;
+
+		if (!(*ff.f).eof())
 		{
-			fout << si.s;
-			fout << endl;
-		}
-		if (!(*si.f).eof())
-		{
-			getline(*si.f, si.s);
-			pq.push(si);
+			getline(*ff.f, ff.str);
+			end_sorting.push(ff);
 		}
 		else
 		{
-			(*(si.f)).close();
+			(*(ff.f)).close();
 		}
 	}
-	for (size_t i = 0; i < k; ++i)
-	{
-		remove((to_string(i + 1) + ".txt").c_str());
+	f12.close();
+	remove_temp_files();
+	
+}
+
+
+inline auto B::division()->void {
+	string line_of_file;
+	size_t temp_size_files = 0;
+	while (!file.eof()) {
+		getline(file, line_of_file);
+		temp_size_files += line_of_file.size();
+
+
+		if (temp_size_files <= buffer) {
+			lines.push_back(line_of_file);
+		}
+		else {
+			count_of_files++;
+			make_file(to_string(count_of_files) + ".txt");
+			lines.push_back(line_of_file);
+			temp_size_files = line_of_file.size();
+		}
 	}
-	fout.close();
+	file.close();
+
+	if (lines.size()) {
+		count_of_files++;
+		make_file(to_string(count_of_files) + ".txt");
+	}
+
+	file_sort();
 }
