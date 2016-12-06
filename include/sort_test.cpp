703efcf6cfd423be96361c9f7bb962910a859e2a
AlexYshkevich <bor_bor_1997@mail.ru>
2016-12-07 02:12:22 +0300

    Update sort_test.cpp

--- include/sort_test.cpp
+++ include/sort_test.cpp
@@ -7,6 +7,7 @@
 #include <algorithm> 
 #include <cstdio> 
 #include <queue> 
+
 using namespace std;
 
 struct A {
@@ -20,16 +21,15 @@ public:
 	}
 };
 
-class RUNNER {
+class B {
 public:
-	
-	RUNNER(string name_main_file,size_t buff_size);
+	B(string name_main_file,size_t buff_size);
 	auto division()->void;
 	auto file_size(string name_file)->size_t;
 	auto make_file(string name_file)->void;
 	auto file_sort()->void;
 	auto remove_temp_files()->void;
-	~RUNNER();
+	~B();
 private:
 	fstream file;
 	size_t buffer, count_of_files, closed_files;
@@ -38,17 +38,19 @@ private:
 	priority_queue<A> end_sorting;
 };
 
-inline RUNNER::~RUNNER() {
+
+
+inline B::~B() {
 	file_names.clear();
 }
 
-inline RUNNER::RUNNER(string name_main_file,size_t buff_size) :file(name_main_file), buffer(buff_size), count_of_files(0), closed_files(0) {
+inline B::B(string name_main_file,size_t buff_size) :file(name_main_file), buffer(buff_size), count_of_files(0), closed_files(0) {
 	if (file.is_open()) {
 		division();
 	}
 };
 
-inline auto RUNNER::make_file(string name_file)->void {
+inline auto B::make_file(string name_file)->void {
 	file_names.push_back(name_file);
 	std::sort(lines.begin(), lines.end());
 	ofstream temp(name_file);
@@ -61,7 +63,7 @@ inline auto RUNNER::make_file(string name_file)->void {
 	lines.clear();
 }
 
-inline auto RUNNER::file_size(string name_file)->size_t {
+inline auto B::file_size(string name_file)->size_t {
 	long fsize;
 	ifstream temp(name_file);
 	temp.seekg(0, ios::end);
@@ -70,19 +72,26 @@ inline auto RUNNER::file_size(string name_file)->size_t {
 	return fsize;
 }
 
-inline auto RUNNER::remove_temp_files()->void {
+
+
+inline auto B::remove_temp_files()->void {
 	for (int i = 0; i < file_names.size(); ++i) {
 		if (remove(file_names[i].c_str()) == -1) {
 			throw;
 		}
 		else {
-			cout << "MS";
+			cout << "Gj";
 		}
 	}
+
 }
 
-inline auto RUNNER::file_sort()->void {
+
+
+
+inline auto B::file_sort()->void {
 	ofstream f12("out.txt");
+	
 	string str;
 	for (int i = 0; i < count_of_files; ++i) {
 		ifstream* f_ = new ifstream(file_names[i]);
@@ -90,10 +99,12 @@ inline auto RUNNER::file_sort()->void {
 		A ff(str, f_);
 		end_sorting.push(ff);
 	}
+	
 	while (!end_sorting.empty()) {
 		A ff = end_sorting.top();
 		end_sorting.pop();
 		if (ff.str != "") f12 << ff.str << endl;
+
 		if (!(*ff.f).eof())
 		{
 			getline(*ff.f, ff.str);
@@ -106,14 +117,18 @@ inline auto RUNNER::file_sort()->void {
 	}
 	f12.close();
 	remove_temp_files();
+	
 }
 
-inline auto RUNNER::division()->void {
+
+inline auto B::division()->void {
 	string line_of_file;
 	size_t temp_size_files = 0;
 	while (!file.eof()) {
 		getline(file, line_of_file);
 		temp_size_files += line_of_file.size();
+
+
 		if (temp_size_files <= buffer) {
 			lines.push_back(line_of_file);
 		}
@@ -125,9 +140,11 @@ inline auto RUNNER::division()->void {
 		}
 	}
 	file.close();
+
 	if (lines.size()) {
 		count_of_files++;
 		make_file(to_string(count_of_files) + ".txt");
 	}
+
 	file_sort();
 }
